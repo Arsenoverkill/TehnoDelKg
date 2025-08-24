918b54e51339d65d34e7fb28af34df788291f219
Arsenoverkill <arsenprogrammer007@gmail.com>
2025-08-24 18:40:56 +0600

    fix all bags

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -46,14 +46,34 @@ function Footer() {
         <div className={styles.bottom}>
           <p>Технодел © 2025</p>
           <div className={styles.social_icons}>
-            <img src={Whatsapp} alt="WhatsApp" className={styles.social_icon} />
+            <a
+              href="https://wa.me/996501488113"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={Whatsapp}
+                alt="WhatsApp"
+                className={styles.social_icon}
+              />
+            </a>
             <img src={Facebook} alt="Facebook" className={styles.social_icon} />
             <img
               src={Instagram}
               alt="Instagram"
               className={styles.social_icon}
             />
-            <img src={Telegram} alt="Telegram" className={styles.social_icon} />
+            <a
+              href="https://t.me/+0501488113"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={Telegram}
+                alt="Telegram"
+                className={styles.social_icon}
+              />
+            </a>
           </div>
         </div>
       </div>
